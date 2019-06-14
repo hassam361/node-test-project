1b8a6f3c91cb8f3e18b8855a3f5d34521824d0d3
hassam asif <hassam.asif48@gmail.com>
2019-06-14 13:12:55 +0500

    setup start and heroku port

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express= require('express');
 const hbs=require('hbs');
+const port =process.env.port || 3000 ;
 var app=express();
 hbs.registerPartials(__dirname+'/views/partials');
 hbs.registerHelper('getCurrentYear',()=>{
@@ -27,4 +28,4 @@ app.get('/about',(req,res)=>{
     }
      );
 })
-app.listen(3000); 
+app.listen(port); 
